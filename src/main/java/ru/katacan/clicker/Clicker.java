a6f07beab05a485644795d4b463d0adb10127eed
Katacan <150337611+katacanXD@users.noreply.github.com>
2025-09-01 23:43:33 +0300

    update

---
+++ src/main/java/ru/katacan/clicker/Clicker.java
@@ -0,0 +1,67 @@
+package ru.katacan.clicker;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
+import ru.katacan.clicker.c.cControls;
+import ru.katacan.clicker.gui.ExampleScreen;
+import ru.katacan.clicker.gui.SettingsGui;
+
+import static ru.katacan.clicker.Config.load;
+
+
+public class Clicker implements ModInitializer {
+    public static final String MOD_ID = "clicker";
+
+    public static Identifier id(String path) {
+        return Identifier.of("clicker", path);
+    }
+
+    private static KeyBinding registerKey(String keyname, int key) {
+
+        return KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key." + MOD_ID + "." + keyname,
+                        InputUtil.Type.KEYSYM,
+                        key, MOD_ID + ".midnightconfig.title"
+                ));
+
+    }
+
+    private void clientStart(MinecraftClient client) {
+        load();
+    }
+
+    @Override
+    public void onInitialize() {
+        ClientLifecycleEvents.CLIENT_STARTED.register(this::clientStart);
+
+        Config.lClickDelayInt = 10;
+
+        KeyBinding open = registerKey("open", GLFW.GLFW_KEY_B);
+        KeyBinding start = registerKey("start", GLFW.GLFW_KEY_F6);
+
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            cControls.Update();
+            Config.save();
+            while (open.wasPressed()) {
+                MinecraftClient.getInstance().setScreen(new ExampleScreen(new SettingsGui()));
+                assert MinecraftClient.getInstance().currentScreen != null;
+                MinecraftClient.getInstance().currentScreen.close();
+                MinecraftClient.getInstance().setScreen(new ExampleScreen(new SettingsGui()));
+            }
+            while (start.wasPressed()) {
+                Config.lTimer = 0;
+                Config.rTimer = 0;
+                cControls.setActive(!cControls.getActive());
+            }
+        });
+    }
+}
---
+++ src/main/java/ru/katacan/clicker/c/cControls.java
@@ -0,0 +1,90 @@
+package ru.katacan.clicker.c;
+
+import io.github.cottonmc.cotton.gui.client.CottonHud;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import ru.katacan.clicker.Config;
+import ru.katacan.clicker.c.gui.Hud;
+
+public class cControls {
+
+    public static void Update() {
+        if (getActive()) {
+            assert MinecraftClient.getInstance().player != null;
+            if (Config.lActive) {
+                CottonHud.add(Hud.item, Hud.itemPositioner());
+                if (!Config.lHoldMode) {
+                    if (!Config.lTargetEntityMode) {
+                        if (Config.lCooldownAttackMode) {
+                            if (MinecraftClient.getInstance().player.getAttackCooldownProgress(0) == 1) {
+                                KeyBinding.onKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_1));
+                            }
+                        } else {
+                            if (Config.lTimer < Config.lClickDelayInt) Config.lTimer++;
+                            else {
+                                KeyBinding.onKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_1));
+                                Config.lTimer = 0;
+                            }
+                        }
+                    } else {
+                        if (MinecraftClient.getInstance().targetedEntity != null) {
+                            if (Config.lCooldownAttackMode) {
+                                if (MinecraftClient.getInstance().player.getAttackCooldownProgress(0) == 1) {
+                                    KeyBinding.onKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_1));
+                                }
+                            } else {
+                                if (Config.lTimer < Config.lClickDelayInt) Config.lTimer++;
+                                else {
+                                    KeyBinding.onKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_1));
+                                    Config.lTimer = 0;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    KeyBinding.setKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_1), true);
+                    Config.lTimer = 0;
+                }
+            }
+            if (Config.rActive) {
+                CottonHud.add(Hud.item, Hud.itemPositioner());
+                if (!Config.rHoldMode) {
+                    if (!Config.rTargetEntityMode) {
+                        if (Config.rTimer < Config.rClickDelayInt) Config.rTimer++;
+                        else {
+                            KeyBinding.onKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_2));
+                            Config.rTimer = 0;
+                        }
+                    } else {
+                        if (MinecraftClient.getInstance().targetedEntity != null) {
+                            if (Config.rTimer < Config.rClickDelayInt) Config.rTimer++;
+                            else {
+                                KeyBinding.onKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_2));
+                                Config.rTimer = 0;
+                            }
+                        }
+                    }
+                } else {
+                    KeyBinding.setKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_2), true);
+                }
+            }
+        }
+    }
+
+    public static boolean getActive() {
+        return Config.Active;
+    }
+
+    public static void setActive(boolean active) {
+        if (!active) {
+            if (Config.lHoldMode)
+                KeyBinding.setKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_1), false);
+            if (Config.rHoldMode)
+                KeyBinding.setKeyPressed(InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_2), false);
+        }
+        Config.Active = active;
+        if (!Config.Active) CottonHud.remove(Hud.item);
+    }
+}
